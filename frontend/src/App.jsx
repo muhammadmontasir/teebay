3bc5b764ad183ce53d78c513febc7ac750830999
montasir-rahman <m.montasir97@gmail.com>
2024-08-26 03:44:47 +0600

    feat(frontend): add component routes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,40 @@
+import React from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { client } from 'Apollo';
-import { Layout } from 'Components/Layout';
-import { Home, Login } from 'Pages';
-import * as React from 'react';
+import { client } from './Apollo';
+import Layout from './components/Layout/index.jsx';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import RoutePaths from './RoutePath';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import CreateProduct from './pages/Products/create/index.jsx';
+import EditProduct from './pages/Products/edit/index.jsx';
+import ProductsList from './pages/Products/index.jsx';
+import { PrimeReactProvider } from 'primereact/api';
 
 const App = () => {
     return (
         <ApolloProvider client={client}>
-            <Router>
-                <Routes>
-                    <Route path={RoutePaths.Login} element={<Login />} />
+            <PrimeReactProvider>
+                <Router>
+                    <Routes>
+                        {/* Route for SignIn page */}
+                        <Route path={RoutePaths.Login} element={<SignIn />} />
 
-                    <Route element={<Layout />}>
-                        <Route path={RoutePaths.Home} element={<Home />} />
-                        <Route path={RoutePaths.About} element={<About />} />
-                    </Route>
-                </Routes>
-            </Router>
+                        {/* Routes that share a common Layout */}
+                        <Route element={<Layout />}>
+                            {/* Route for SignUp page */}
+                            <Route path={RoutePaths.SignUp} element={<SignUp />} />
+                            <Route path={RoutePaths.ProductList} element={<ProductsList />} />
+
+                            {/* Route for CreateProduct page */}
+                            <Route path={RoutePaths.CreateProduct} element={<CreateProduct />} />
+
+                            {/* Route for EditProduct page with dynamic id */}
+                            <Route path={RoutePaths.EditProduct} element={<EditProduct />} />
+                        </Route>
+                    </Routes>
+                </Router>
+                </ PrimeReactProvider>
         </ApolloProvider>
     );
 };
